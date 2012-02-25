0c7e887ed8a8197df67668391f2340414bfb5a16
Scott Miles <scott.miles@palm.com>
2012-02-25 14:19:56 -0800

    add 'stretch' option to fittable with example

--- fittable/Fittable.js
+++ fittable/Fittable.js
@@ -1,9 +1,12 @@
 enyo.kind({
 	name: "enyo.Fittable",
+	published: {
+		stretch: true
+	},
 	classes: "enyo-fittable",
 	components: [
 		// box offsetHeight equals Rows content height, which is otherwise unavailable with calculations
-		{name: "box", classes: "enyo-0", components: [
+		{name: "box", classes: "enyo-0 enyo-stretch", components: [
 			// top offsetHeight will be sum of child heights (including child pad-border-margin) which otherwise requires calculations
 			{name: "pre", classes: "enyo-0"},
 			// flex can be sized without regard to child pad-border-margin, sizing child directly would require calculations
@@ -12,6 +15,15 @@ enyo.kind({
 			{name: "post", classes: "enyo-0"}
 		]}
 	],
+	create: function() {
+		this.inherited(arguments);
+		if (!this.stretch) {
+			this.stretchChanged();
+		}
+	},
+	stretchChanged: function() {
+		this.$.box.addRemoveClass("enyo-stretch", this.stretch);
+	},
 	flow: function() {
 		var recontain = function(o, c) {
 			if (o.container != c) {
