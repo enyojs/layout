1c1810e174cb23e0cdc4a1ad1327bc8c14d362e4
germboy <jeremy.t@verizon.net>
2012-06-28 00:52:00 -0400

    Add twiddle fix for Android 4.0.4 refresh bug
    JIRA ENYO-310

--- list/source/List.js
+++ list/source/List.js
@@ -304,6 +304,11 @@ enyo.kind({
 		this.invalidatePages();
 		this.update(this.getScrollTop());
 		this.stabilize();
+
+		//FIXME: Necessary evil for Android 4.0.4 refresh bug
+		if (enyo.platform.android === 4) {
+			this.twiddle();
+		}
 	},
 	//* Re-render the list from the beginning
 	reset: function() {
