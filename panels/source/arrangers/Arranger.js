a2a1cbe17b1278251ba429ddb7127c31119203e3
Jim Tang <jdtang@cww.org>
2012-07-02 16:40:47 -0700

    General editing, plus moving content into Dev Guide

--- panels/source/arrangers/Arranger.js
+++ panels/source/arrangers/Arranger.js
@@ -1,50 +1,34 @@
 /**
-	enyo.Arranger is an enyo.Layout that considers one of the controls it lays out as active.
-	The other controls are placed relative to the active control as makes sense for the layout.
+	_enyo.Arranger_ is an <a href="#enyo.Layout">enyo.Layout</a> that considers
+	one of the controls it lays out as active. The other controls are placed
+	relative to the active control as makes sense for the layout.
 
-	Arranger supports dynamic layouts, meaning it's possible to transition between its layouts
-	via animation. Typically arrangers should layout controls using css transforms since 
-	they are optimized for animation. To support this, controls in an Arranger are absolutely
-	positioned, and arranger has an accelerated setting which will mark controls to
-	receive css compositing. The default setting of "auto" ensure this will occur if 
-	enabled by the platform.
+	Arranger supports dynamic layouts, meaning it's possible to transition
+	between its layouts	via animation. Typically, arrangers should lay out
+	controls using CSS transforms, since these are optimized for animation. To
+	support this, the controls in an Arranger are absolutely positioned, and
+	the Arranger kind has an `accelerated` property, which marks controls for
+	CSS compositing. The default setting of "auto" ensures that this will occur
+	if enabled by the platform.
 	
-	Arranger has methods to support dynamic layouts. Arranger subkinds are typically used as layouts
-	for an <a href="#enyo.Panels">enyo.Panels</a> which uses the Arranger api to implement transitions
-	between layouts. Implement the size method to size controls and perform other expensive layout
-	operations that are only required when the layout reflows. Implement the start and finish methods to
-	specify behavior that should occur when a transition begins and ends. The start method
-	can set the transitionPoints array on the layout container. These are the arrangements between which
-	the layout should transition.
-	
-	Implement the arrange method to position controls relative to each other. The arrange method is 
-	where the real layout work occurs. An arranger need only specify the layout for each active position
-	and need not concern itself with intermediate layouts. Instead of directly applying styling to controls
-	in the arrange method, the arrangeControl(inControl, inArrangement) method should be used.
-	The inArrangement argument is an object with settings that are later applied to the control via the 
-	flowControl(inControl, inArrangement) method. By default, the flowControl method will process arrangement settings
-	for left, top, and opacity, and this method should only be implemented to style the control in other ways.
-
-	Arranger also supports dragging between active states. The arranger must specify how dragging will effect
-	the layout. The dragProp, canDragProp, and dragDirectionProp are used to indicate which direction of drag
-	is relevant. The calcArrangementDifference(inI0, inA0, inI1, inA1) method should be implemented to return the 
-	difference measured in pixels between the arrangement inA0 for layout setting inI0 and arrangement inA1 for layout
-	setting inI1. This data is used to calculate the percent a drag should move the layout between two active states.
+	For more information, see the
+	<a href="https://github.com/enyojs/enyo/wiki/Arrangers">documentation for Arrangers</a>
+	in the Enyo	Developer Guide.
 */
 enyo.kind({
 	name: "enyo.Arranger",
 	kind: "Layout",
 	layoutClass: "enyo-arranger",
 	/** 
-		Sets controls being laid out to use css compositing. A setting of auto will mark 
-		controls for compositing if the platform supports it.
+		Sets controls being laid out to use CSS compositing. A setting of "auto"
+		will mark controls for compositing if the platform supports it.
 	*/
 	accelerated: "auto",
-	//* Property of the drag event that should be used to calculate the amount a drag moves the layout.
+	//* Property of the drag event used to calculate the amount a drag moves the layout
 	dragProp: "ddx",
-	//* Property of the drag event that should be used to calculate the direction of a drag.
+	//* Property of the drag event used to calculate the direction of a drag
 	dragDirectionProp: "xDirection",
-	//* Property of the drag event that should be used to calculate if a drag should occur.
+	//* Property of the drag event used to calculate whether a drag should occur
 	canDragProp: "horizontal",
 	//* @protected
 	destroy: function() {
@@ -56,24 +40,28 @@ enyo.kind({
 	},
 	//* @public
 	/**
-		Arrange the given array of controls (inC) in the layout specified by inName.
-		When implementing this method, rather than apply styling directly to controls, use the
-		arrangeControl(inControl, inArrangement) method with an inArrangement object with styling settings.
-		These will later be applied via the flowControl(inControl, inArrangement) method.
+		Arranges the given array of controls (_inC_) in the layout specified by
+		_inName_. When implementing this method, rather than apply styling
+		directly to controls, call _arrangeControl(inControl, inArrangement)_
+		with an _inArrangement_	object with styling settings. These will then be
+		applied via the	_flowControl(inControl, inArrangement)_ method.
 	*/
 	arrange: function(inC, inName) {
 	},
 	/**
-		Sizes the controls in the layout. This method is called only at reflow time. Note, sizing is separated 
-		from other layout done in the arrange method because it is expensive and not suitable for dynamic layout.
+		Sizes the controls in the layout. This method is called only at reflow
+		time. Note that sizing is separated from other layout done in the
+		_arrange_ method because it is expensive and not suitable for dynamic
+		layout.
 	*/
 	size: function() {
 	},
 	/**
-		Called when a layout transition begins. Implement this method to perform tasks which should only occur
-		when a transition starts; for example, some controls could be shown or hidden. In addition, the 
-		transitionPoints array may be set on the container to dictate between which named arrangments
-		the transition occurs.
+		Called when a layout transition begins. Implement this method to perform
+		tasks that should only occur when a transition starts; for example, some
+		controls could be shown or hidden. In addition, the	_transitionPoints_
+		array may be set on the container to dictate the named arrangments
+		between which the transition occurs.
 	*/
 	start: function() {
 		var f = this.container.fromIndex, t = this.container.toIndex;
@@ -91,8 +79,9 @@ enyo.kind({
 		p$.push(this.container.toIndex);
 	},
 	/**
-		Called when a layout transition completes. Implement this method to perform tasks which should only occur
-		when a transition ends; for example, some controls could be shown or hidden.
+		Called when a layout transition completes. Implement this method to
+		perform tasks that should only occur when a transition ends; for
+		example, some controls could be shown or hidden.
 	*/
 	finish: function() {
 	},
@@ -119,10 +108,10 @@ enyo.kind({
 	},
 	//* @public
 	/**
-	This method is called when dragging the layout, and it should return the the 
-	difference measured in pixels between the arrangement inA0 for layout setting inI0 
-	and arrangement inA1 for layout setting inI1. This data is used to calculate 
-	the percent a drag should move the layout between two active states.
+	Called when dragging the layout, this method returns the difference in
+	pixels between the arrangement _inA0_ for layout setting _inI0_	and
+	arrangement _inA1_ for layout setting _inI1_. This data is used to calculate
+	the percentage that a drag should move the layout between two active states.
 	*/
 	calcArrangementDifference: function(inI0, inA0, inI1, inA1) {
 	},
@@ -156,9 +145,10 @@ enyo.kind({
 	},
 	//* @public
 	/**
-		Layout the control (inControl) according to the settings stored in the inArrangment object.
-		By default, flowControl will apply settings of left, top, and opacity. This method should 
-		only be implemented to apply other settings made via arrangeControl.
+		Lays out the control (_inControl_) according to the settings stored in
+		the	_inArrangment_ object. By default, _flowControl_ will apply settings
+		of left, top, and opacity. This method should only be implemented to
+		apply other settings made via _arrangeControl_.
 	*/
 	flowControl: function(inControl, inArrangement) {
 		enyo.Arranger.positionControl(inControl, inArrangement);
@@ -168,7 +158,7 @@ enyo.kind({
 		}
 	},
 	//* @protected
-	// get an array of controls arranged in state order.
+	// Gets an array of controls arranged in state order.
 	// note: optimization, dial around a single array.
 	getOrderedControls: function(inIndex) {
 		var whole = Math.floor(inIndex);
