afc8aa1f99231c410f58850489efb303b01c8905
unwiredben <combee@techwood.org>
2012-12-19 00:36:03 -0600

    Remove left-over swipeable/reorder code from Contacts sample, fix problem where tapping in setup sliders didn't change display
    Enyo-DCO-1.1-Signed-Off-By: Ben Combee (ben.combee@palm.com)

--- list/samples/ListContactsSample.js
+++ list/samples/ListContactsSample.js
@@ -16,41 +16,24 @@ enyo.kind({
 			]},
 			{kind: "onyx.Button", content: "remove selected", ontap: "removeSelected"}
 		]},
-		{kind: "List", classes: "list-sample-contacts-list enyo-unselectable", fit: true, multiSelect: true, //reorderable: true,
-			onSetupItem: "setupItem", /*onSetupReorderComponents: "setupReorderComponents", onSetupPinnedReorderComponents: "setupPinnedReorderComponents", onReorder: "listReorder",
-			onSetupSwipeItem: "setupSwipeItem", onSwipeComplete: "swipeComplete", */components: [
+		{kind: "List", classes: "list-sample-contacts-list enyo-unselectable", fit: true, multiSelect: true,
+			onSetupItem: "setupItem", components: [
 				{name: "divider", classes: "list-sample-contacts-divider"},
-				{name: "item", kind: "ContactItem", classes: "list-sample-contacts-item enyo-border-box", onRemove: "removeTap"},
-			]/*, reorderComponents: [
-				{name: "reorderContent", style:"background:rgba(0,0,0,0.7);margin:0px;padding:0px;", classes: "enyo-fit", components: [
-					{name: "reorderTitle", tag: "h2", style: "text-align:center;color:#fff;font-size:24px;"}
-				]}
-			], pinnedReorderComponents: [
-				{name: "pinnedReorderContent", style:"background:rgba(150,0,0,0.7);margin:0px;padding:0px;", classes: "enyo-fit", components: [
-					{name: "pinnedReorderTitle", tag: "h2", style: "text-align:center;color:#fff;font-size:24px;"},
-					{name: "dropButton", kind: "onyx.Button", ontap: "dropPinnedRow", content: "Drop", style: "width:100px;height:80px;position:absolute;top:10px;right:20px;"}
-				]}
-			], swipeableComponents: [
-				{name: "importanceSwipeItem", style:"background:red;color:white;", classes: "enyo-fit", components: [
-					{name: "importanceSwipeTitle", content: "Important!", style: "font-size:30px;font-weight:bold;color:#fff;text-align:center;line-height:100px;padding:0px;margin:0px;"}
-				]},
-				{name: "deleteSwipeItem", style:"background:rgba(200,0,0,0.9);color:white;height:100px;", components: [
-					{name:"deleteButton", kind:"onyx.Button", content:"Delete", style:"height:60px;width:200px;display:block;margin:0px auto;position:relative;top:20px;line-height:60px;padding:0px;border:1px solid yellow;", ontap:"clearSwipeables"}
-				]}
-			]*/
+				{name: "item", kind: "ContactItem", classes: "list-sample-contacts-item enyo-border-box", onRemove: "removeTap"}
+			]
 		},
 		{name: "popup", kind: "onyx.Popup", modal: true, centered: true, classes: "list-sample-contacts-popup", components: [
 			{components: [
-				{style:"display:inline-block", components:[
+				{style: "display:inline-block", components:[
 					{content: "count:", classes: "list-sample-contacts-label"},
-					{name:"countOutput", style:"display:inline-block;", content: "200"}
+					{name: "countOutput", style: "display:inline-block;", content: "200"}
 				]},
-				{kind: "onyx.Slider", value: 4, onChanging:"countSliderChanging"}
+				{kind: "onyx.Slider", value: 4, onChanging: "countSliderChanging", onChange: "countSliderChanging"}
 			]},
 			{components: [
 				{content: "rowsPerPage:", classes: "list-sample-contacts-label"},
-				{name:"rowsPerPageOutput", style:"display:inline-block;", content: "50"},
-				{kind: "onyx.Slider", value: 10, onChanging:"rowsSliderChanging"}
+				{name: "rowsPerPageOutput", style: "display:inline-block;", content: "50"},
+				{kind: "onyx.Slider", value: 10, onChanging: "rowsSliderChanging", onChange: "rowsSliderChanging"}
 			]},
 			{components: [
 				{content: "hide divider:", classes: "list-sample-contacts-label"},
@@ -61,28 +44,6 @@ enyo.kind({
 			]}
 		]}
 	],
-	setupPinnedReorderComponents: function(inSender, inEvent) {
-		var i = inEvent.index;
-		var data = this.filter ? this.filtered : this.db;
-		var item = data[i];
-		this.$.pinnedReorderTitle.setContent("Pinned - "+item.name);
-	},
-	setupReorderComponents: function(inSender, inEvent) {
-		var i = inEvent.index;
-		var data = this.filter ? this.filtered : this.db;
-		var item = data[i];
-		if(!item) {
-			return;
-		}
-		this.$.reorderTitle.setContent("You are moving - "+item.name);
-	},
-	listReorder: function(inSender, inEvent) {
-		var data = this.filter ? this.filtered : this.db;
-		var movedItem = enyo.clone(data[inEvent.reorderFrom]);
-		data.splice(inEvent.reorderFrom,1);
-		data.splice((inEvent.reorderTo),0,movedItem);
-		enyo.log("from: "+inEvent.reorderFrom+", to: "+inEvent.reorderTo);
-	},
 	rendered: function() {
 		this.inherited(arguments);
 		this.populateList();
@@ -95,9 +56,6 @@ enyo.kind({
 		this.$.item.setContact(item);
 		// selection
 		this.$.item.setSelected(inSender.isSelected(i));
-		// importance
-		//this.$.item.setImportance(item.importance);
-		//this.$.item.renderImportance();
 		// divider
 		if (!this.hideDivider) {
 			var d = item.name[0];
@@ -107,10 +65,6 @@ enyo.kind({
 			this.$.divider.canGenerate = showd;
 			this.$.item.applyStyle("border-top", showd ? "none" : null);
 		}
-	},
-	//* Called when the "Drop" button is pressed on the pinned placeholder row
-	dropPinnedRow: function(inSender, inEvent) {
-		this.$.list.dropPinnedRow(inEvent);
 	},
 	refreshList: function() {
 		if (this.filter) {
@@ -226,91 +180,7 @@ enyo.kind({
 	},
 	rowsSliderChanging: function(inSender, inEvent){
 		this.$.rowsPerPageOutput.setContent(Math.round(inSender.getValue()) * 5);
-	},
-	
-	
-	//
-	// swipeable interface
-	//
-	setupSwipeItem: function(inSender, inEvent) {
-		var i = inEvent.index;
-		var data = this.filter ? this.filtered : this.db;
-		var item = data[i];
-		
-		if(!item) {
-			return;
-		}
-		
-		item.importance = item.importance || 0;
-
-		// if swiped to the right
-		if(inEvent.xDirection == 1) {
-			this.$.list.setEnableSwipe(true);
-			this.$.deleteSwipeItem.setShowing(false);
-			this.$.importanceSwipeItem.setShowing(true);
-			if(item.importance == 0) {
-				this.$.importanceSwipeItem.applyStyle("background-color","rgba(255,140,0,0.8)");
-				this.$.importanceSwipeTitle.setContent("Important!");
-			}
-			else if(item.importance == -1) {
-				this.$.importanceSwipeItem.applyStyle("background-color","rgba(0,160,40,0.8)");
-				this.$.importanceSwipeTitle.setContent("Very Important!!");
-			}
-			else if(item.importance == -2) {
-				this.$.importanceSwipeItem.applyStyle("background-color","rgba(0,0,255,0.8)");
-				this.$.importanceSwipeTitle.setContent("Not Important");
-			}
-		// if swiped to the left
-		} else {
-			this.$.list.setEnableSwipe(true);
-			if(item.importance < 0) {
-				this.$.importanceSwipeItem.applyStyle("background-color","rgba(0,0,255,0.8)");
-				this.$.importanceSwipeTitle.setContent("Not Important");
-				this.$.deleteSwipeItem.setShowing(false);
-				this.$.importanceSwipeItem.setShowing(true);
-			} else {
-				this.$.importanceSwipeItem.setShowing(false);
-				this.$.deleteSwipeItem.setShowing(true);
-				this.$.list.setPersistSwipeableItem(true);
-			}
-		}
-	},
-	swipeComplete: function(inSender, inEvent) {
-		var i = inEvent.index;
-		var data = this.filter ? this.filtered : this.db;
-		var item = data[i];
-		
-		if(!item) {
-			return;
-		}
-		
-		item.importance = item.importance || 0;
-		
-		// if swiped to the right
-		if(inEvent.xDirection == 1) {
-			if(item.importance == 0) {
-				item.importance = -1;
-			}
-			else if(item.importance == -1) {
-				item.importance = -2;
-			}
-			else if(item.importance == -2) {
-				item.importance = 0;
-			}
-		// if swiped to the left
-		} else {
-			if(item.importance < 0) {
-				item.importance = 0;
-			}
-		}
-		this.$.list.updateCurrentRow();
-	},
-	clearSwipeables: function() {
-		this.$.list.clearSwipeables();
 	}
-	//
-	// end swipeable interface
-	//
 });
 
 var avatars = [
@@ -355,8 +225,7 @@ enyo.kind({
 		{components: [
 			{name: "name"},
 			{name: "title", classes: "list-sample-contacts-description"},
-			{content: "(415) 711-1234", classes: "list-sample-contacts-description"}//,
-			//{name: "importance", content: "not important"}
+			{content: "(415) 711-1234", classes: "list-sample-contacts-description"}
 		]},
 		{name: "remove", kind: "onyx.IconButton", classes: "list-sample-contacts-remove-button", src: "assets/remove-icon.png", ontap: "removeTap"}
 	],
