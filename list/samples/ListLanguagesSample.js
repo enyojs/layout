1bb1ca1a151fa2435ab64e11d410b12b048f0154
unwiredben <combee@techwood.org>
2012-12-20 02:55:20 -0600

    Update List Language sample with abilty to set row count for boundary testing, also remove global variable use
    Enyo-DCO-1.1-Signed-Off-By: Ben Combee (ben.combee@palm.com)

--- list/samples/ListLanguagesSample.js
+++ list/samples/ListLanguagesSample.js
@@ -2,9 +2,21 @@ enyo.kind({
 	name: "enyo.sample.ListLanguagesSample",
 	kind: "FittableRows",
 	classes: "list-sample-language enyo-fit",
+	data: [],
+	languages: {
+		English: ["One",  "Two",  "Three", "Four",    "Five",      "Six",   "Seven",  "Eight", "Nine",  "Ten"],
+		Italian: ["Uno",  "Due",  "Tre",   "Quattro", "Cinque",    "Sei",   "Sette",  "Otto",  "Nove",  "Dieci"],
+		Spanish: ["Uno",  "Dos",  "Tres",  "Cuatro",  "Cinco",     "Seis",  "Siete",  "Ocho",  "Nueve", "Diez"],
+		German:  ["Eins", "Zwei", "Drei",  "Vier",    "F&uuml;nf", "Sechs", "Sieben", "Acht",  "Neun",  "Zehn"],
+		French:  ["Un",   "Deux", "Trois", "Quatre",  "Cinq",      "Six",   "Sept",   "Huit",  "Neuf",  "Dix"]
+	},
 	components: [
 		{kind: "onyx.MoreToolbar", layoutKind: "FittableColumnsLayout", style: "height: 55px;", components: [
 			{kind: "onyx.Button", content: "Randomize", ontap: "populateList"},
+			{content: "Number of Rows:"},
+			{kind: "onyx.InputDecorator", components: [
+				{kind: "onyx.Input", value: "10", name: "numRows" }
+			]}
 		]},
 		{kind: "List", classes: "list-sample-language-list enyo-unselectable", fit: true, multiSelect: true, reorderable: true,
 			onSetupItem: "setupItem", onReorder: "listReorder", onSetupReorderComponents: "setupReorderComponents", onSetupPinnedReorderComponents: "setupPinnedReorderComponents",
@@ -35,38 +47,38 @@ enyo.kind({
 		this.populateList();
 	},
 	listReorder: function(inSender, inEvent) {
-		var movedItem = enyo.clone(data[inEvent.reorderFrom]);
-		data.splice(inEvent.reorderFrom,1);
-		data.splice((inEvent.reorderTo),0,movedItem);
+		var movedItem = enyo.clone(this.data[inEvent.reorderFrom]);
+		this.data.splice(inEvent.reorderFrom,1);
+		this.data.splice((inEvent.reorderTo),0,movedItem);
 	},
 	setupItem: function(inSender, inEvent) {
 		var i = inEvent.index;
-		if(!data[i]) {
+		if(!this.data[i]) {
 			return;
 		}
-		var currentLanguage = data[i].langs[data[i].currentIndex];
-		var val = data[i].val;
-		var number = languages[currentLanguage][val];
+		var currentLanguage = this.data[i].langs[this.data[i].currentIndex];
+		var val = this.data[i].val;
+		var number = this.languages[currentLanguage][val];
 		this.$.text.setContent(number);
 	},
 	setupReorderComponents: function(inSender, inEvent) {
 		var i = inEvent.index;
-		if(!data[i]) {
+		if(!this.data[i]) {
 			return;
 		}
-		var currentLanguage = data[i].langs[data[i].currentIndex];
-		var val = data[i].val;
-		var number = languages[currentLanguage][val];
+		var currentLanguage = this.data[i].langs[this.data[i].currentIndex];
+		var val = this.data[i].val;
+		var number = this.languages[currentLanguage][val];
 		this.$.reorderTitle.setContent(number);
 	},
 	setupPinnedReorderComponents: function(inSender, inEvent) {
 		var i = inEvent.index;
-		if(!data[i]) {
+		if(!this.data[i]) {
 			return;
 		}
-		var currentLanguage = data[i].langs[data[i].currentIndex];
-		var val = data[i].val;
-		var number = languages[currentLanguage][val];
+		var currentLanguage = this.data[i].langs[this.data[i].currentIndex];
+		var val = this.data[i].val;
+		var number = this.languages[currentLanguage][val];
 		this.$.pinnedReorderTitle.setContent(number);
 	},
 	//* Called when the "Drop" button is pressed on the pinned placeholder row
@@ -75,66 +87,52 @@ enyo.kind({
 	},
 	setupSwipeItem: function(inSender, inEvent) {
 		var i = inEvent.index;
-		if(!data[i]) {
+		if(!this.data[i]) {
 			return;
 		}
 		var newLang = (inEvent.xDirection == 1)
 			? this.getNextLang(i)
 			: this.getPrevLang(i);
-		this.$.swipeTitle.setContent(data[i].langs[newLang]);
+		this.$.swipeTitle.setContent(this.data[i].langs[newLang]);
 	},
 	swipeComplete: function(inSender, inEvent) {
 		var i = inEvent.index;
-		data[i].currentIndex = (inEvent.xDirection == 1)
+		this.data[i].currentIndex = (inEvent.xDirection == 1)
 			? this.getNextLang(i)
 			: this.getPrevLang(i);
 		this.$.list.renderRow(i);
 	},
 	getNextLang: function(index) {
-		var currentLang = data[index].currentIndex;
-		var nextLang = currentLang + 1;
-		return (nextLang >= data[index].langs.length) ? 0 : nextLang;
+		var currentLang = this.data[index].currentIndex;
+		return (currentLang + 1) % this.data[index].langs.length;
 	},
 	getPrevLang: function(index) {
-		var currentLang = data[index].currentIndex;
-		var prevLang = currentLang - 1;
-		return (prevLang < 0) ? data[index].langs.length - 1 : prevLang;
+		var currentLang = this.data[index].currentIndex;
+		return (currentLang - 1 + this.data[index].langs.length) % this.data[index].langs.length;
 	},
 	populateList: function() {
 		this.createRandomData();
-		this.$.list.setCount(10);
+		this.$.list.setCount(this.data.length);
 		this.$.list.reset();
 	},
 	createRandomData: function() {
 		var languages = this.getLanguages();
 		var langs;
-		data = [];
-		for(var i=0;i<10;i++) {
+		var dataCount = parseInt(this.$.numRows.getValue(), 10);
+		this.data = [];
+		var sortFunc = function() {return 0.5 - Math.random();};
+		for(var i=0;i<dataCount;i++) {
 			langs = enyo.clone(languages);
-			langs.sort(function() {return 0.5 - Math.random()});
-			data.push({
+			langs.sort(sortFunc);
+			this.data.push({
 				langs: langs,
-				val: i,
+				val: i % 10,
 				currentIndex: 0
 			});
 		}
-		data.sort(function() {return 0.5 - Math.random()});
+		this.data.sort(function() {return 0.5 - Math.random();});
 	},
 	getLanguages: function() {
-		var retarr = [];
-		for(var lang in languages) {
-			retarr.push(lang);
-		}
-		return retarr;
+		return enyo.keys(this.languages);
 	}
 });
-
-data = [];
-
-languages = {
-	English: ["One",  "Two",  "Three", "Four",    "Five",      "Six",   "Seven",  "Eight", "Nine",  "Ten"],
-	Italian: ["Uno",  "Due",  "Tre",   "Quattro", "Cinque",    "Sei",   "Sette",  "Otto",  "Nove",  "Dieci"],
-	Spanish: ["Uno",  "Dos",  "Tres",  "Cuatro",  "Cinco",     "Seis",  "Siete",  "Ocho",  "Nueve", "Diez"],
-	German:  ["Eins", "Zwei", "Drei",  "Vier",    "F&uuml;nf", "Sechs", "Sieben", "Acht",  "Neun",  "Zehn"],
-	French:  ["Un",   "Deux", "Trois", "Quatre",  "Cinq",      "Six",   "Sept",   "Huit",  "Neuf",  "Dix"]
-};
